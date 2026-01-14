ea03a0f7f16fc65f28173d542a0b7e6ff0c3b50c
Giancarlo Perez <giancarloperez.cs@gmail.com>
2026-01-14 13:08:58 -0800

    finished footer (still need to add links)

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -8,11 +8,11 @@ export default function Footer(){
             <div className="py-6 px-4 font-bold bg-primary">
                 <p className="font-heading text-light text-3xl">NutriFind</p>
             </div>
-            <div className="py-6 px-4 bg-light-900 ">
+            <div className="py-4 px-4 bg-accent-400 ">
                 <p className="text-sm text-light font-body">Learn more about NutriFind</p>
             </div>
             <hr className="border-light-700"></hr>
-            <div className="py-6 bg-light-900 grid grid-cols-2 font-body text-light text-sm">
+            <div className="py-6 bg-accent-400 grid grid-cols-2 font-body text-light text-sm">
                 <div className="px-4">
                     <p>Terms & Conditions</p>
                     <p>Privacy Policy</p>
@@ -23,7 +23,17 @@ export default function Footer(){
                     <p>Copyright</p>
                 </div>
             </div>
-            <div className="flex flex-row"></div>
+            <hr className="border-light-700"></hr>
+            <div className="flex flex-row bg-accent-400">
+                <FaInstagram className="m-4.5 text-light text-2xl"/>
+                <FaTiktok className="m-4 text-light text-2xl"/>
+                <FaFacebook className="m-4 text-light text-2xl"/>
+                <FaXTwitter className="m-4 text-light text-2xl"/>
+            </div>
+            <hr className="border-light-700"></hr>
+            <div className="flex flex-row bg-accent-400 py-4 px-4 font-body text-light text-sm">
+                <p>© 2026 NutriFind. The NutriFind Company is not responsible for the content of external sites</p>
+            </div>
         </div>
     )
 }
