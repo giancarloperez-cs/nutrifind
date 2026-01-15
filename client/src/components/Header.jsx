220d8c303cb92cfdaff65bfd854bf0388870a4d0
Giancarlo Perez <giancarloperez.cs@gmail.com>
2026-01-15 07:49:29 -0800

    test

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,6 +1,6 @@
 export default function Header({headingText, bodyText, variant="primary", onClick, className=""}){
     return(
-        <div className="bg-primary-600 h-screen-[] py-10">
+        <div className="bg-primary-600 h-screen-[vh-75%] py-10">
 
         </div>
     )
